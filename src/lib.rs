80d0b3f7f5f98a9f8699a26e8808db1c37832d6b
Kyosuke Fujimoto <kyoro.f@gmail.com>
2024-11-24 18:19:16 +0900

    Update document comment

--- src/lib.rs
+++ src/lib.rs
@@ -2,6 +2,8 @@
 //!
 //! # Example
 //!
+//! ## Basic
+//!
 //! Add the `#[optional]` and `#[nested]` attributes as follows:
 //!
 //! ```
@@ -16,7 +18,7 @@
 //!   bar: Bar,
 //! }
 //!
-//! #[optional(derives = ["Debug"])]
+//! #[optional]
 //! #[derive(Default)]
 //! struct Bar {
 //!   name: String,
@@ -33,7 +35,7 @@
 //! #   name: String,
 //! #   bar: Bar,
 //! # }
-//!
+//! #
 //! # #[derive(Default)]
 //! # struct Bar {
 //! #   name: String,
@@ -60,7 +62,6 @@
 //!   }
 //! }
 //!
-//! #[derive(Debug)]
 //! struct OptionalBar {
 //!   name: Option<String>,
 //!   value: Option<i32>,
@@ -68,6 +69,83 @@
 //!
 //! impl From<OptionalBar> for Bar {
 //!   fn from(optional: OptionalBar) -> Self {
+//!       let mut base = Self::default();
+//!       // ...
+//!       base
+//!   }
+//! }
+//! ```
+//!
+//! ## Derives
+//!
+//! By using the `derives` attribute, the derive attribute can be added to the generated struct:
+//!
+//! ```
+//! use umbra::optional;
+//!
+//! #[optional(derives = ["Debug"])]
+//! #[derive(Default)]
+//! struct Bar {
+//!   name: String,
+//!   value: Option<i32>,
+//! }
+//! ```
+//!
+//! The macro generates following structs:
+//!
+//! ```
+//! # #[derive(Default)]
+//! # struct Bar {
+//! #   name: String,
+//! #   value: Option<i32>,
+//! # }
+//! #
+//! #[derive(Debug)] // The derive attribute is added
+//! struct OptionalBar {
+//!   name: Option<String>,
+//!   value: Option<i32>,
+//! }
+//!
+//! impl From<OptionalBar> for Bar {
+//!   fn from(optional: OptionalBar) -> Self {
+//!       let mut base = Self::default();
+//!       // ...
+//!       base
+//!   }
+//! }
+//! ```
+//!
+//! ## Prefix / Suffix
+//!
+//! By using the `prefix` and `suffix` attributes, the prefix and suffix of the generated struct are changed:
+//!
+//! ```
+//! use umbra::optional;
+//!
+//! #[optional(prefix = "Pre", suffix = "Suf")]
+//! #[derive(Default)]
+//! struct Foo {
+//!   id: u32,
+//!   name: String,
+//! }
+//! ```
+//!
+//! The macro generates following structs:
+//!
+//! ```
+//! # #[derive(Default)]
+//! # struct Foo {
+//! #   id: u32,
+//! #   name: String,
+//! # }
+//! #
+//! struct PreFooSuf { // <Prefix><Base name><Suffix> format
+//!   id: Option<u32>,
+//!   name: Option<String>,
+//! }
+//!
+//! impl From<PreFooSuf> for Foo {
+//!   fn from(optional: PreFooSuf) -> Self {
 //!       let mut base = Self::default();
 //!       // ...
 //!       base
